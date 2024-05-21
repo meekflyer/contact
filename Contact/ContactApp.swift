1083a08cab0488d45b61df47eb7553a6fdbfdfcf
Thomas Patrick <thomasgpatrick0203@gmail.com>
2024-05-20 20:28:46 -0400

    Initial Commit

---
+++ Contact/ContactApp.swift
@@ -0,0 +1,32 @@
+//
+//  ContactApp.swift
+//  Contact
+//
+//  Created by Thomas Patrick on 5/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ContactApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Contact/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Contact
+//
+//  Created by Thomas Patrick on 5/20/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
